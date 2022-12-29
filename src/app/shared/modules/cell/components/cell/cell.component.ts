4855f6e899e2a65d3bba29ab52db76627a986fc5
Andrew Baturin <baturin.andrew1501@gmail.com>
2022-12-30 00:54:49 +0200

    refactor: added logic for cell component

--- src/app/shared/modules/cell/components/cell/cell.component.ts
+++ src/app/shared/modules/cell/components/cell/cell.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
+import { Subject, Subscription, takeUntil, timer } from 'rxjs';
+
+import { CellService } from '../../../../services/cell.service';
+
+
+const AMOUNT_TO_WIN: number = 10;
 
 @Component({
   selector: 'app-cell',
   template: `
-    <div class="cell"></div>
+    <div 
+      #cellRef
+      class="cell" 
+      (click)="handleCellClick()"
+    ></div>
   `,
   styles: [`
     .cell {
@@ -19,4 +39,96 @@ import { Component } from '@angular/core';
     }
   `]
 })
-export class CellComponent { }
+export class CellComponent implements OnInit, OnDestroy {
+  @ViewChild('cellRef') public cellRef: ElementRef<HTMLDivElement>;
+
+  @Output() public onEnd: EventEmitter<void> = new EventEmitter<void>();
+
+  @Input() public cellId: number;
+  @Input() public time: number = 0;
+  @Input() public activeCell$: Subject<number>;
+
+  public computerCells: number[];
+  public playerCells: number[];
+
+  private activatedCell: boolean = false;
+  private destroy$: Subject<null> = new Subject<null>();
+
+  public constructor(
+    private renderer: Renderer2,
+    private cellService: CellService
+  ) { }
+
+  public ngOnInit(): void {
+    this.initValues();
+    this.activateCell();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  public handleCellClick(): void {
+    this.checkForVictory();
+    this.activatedCell = true;
+
+    const candidate = this.cellService.checkIncomingNum(this.cellId);
+    if (candidate) {
+      return;
+    }
+
+    this.renderColor('#359b14')
+    this.cellService.activateCellPlayer(this.cellId);
+  }
+
+  private renderColor(color: string): void {
+    this.renderer.setStyle(
+      this.cellRef.nativeElement,
+      'backgroundColor',
+      color
+    );
+  }
+
+  private checkForVictory(): void {
+    if (
+      this.computerCells.length === AMOUNT_TO_WIN ||
+      this.playerCells.length === AMOUNT_TO_WIN
+    ) {
+      this.onEnd.emit();
+    }
+  }
+
+  private countToComputer(): void {
+    this.checkForVictory();
+    this.cellService.activateCellComputer(this.cellId);
+    this.renderColor('#ab0404');
+  }
+
+  private startTimer(): void {
+    timer(this.time * 1000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        if (!this.activatedCell) {
+          this.countToComputer();
+        }
+      });
+    }
+
+  private activateCell(): Subscription {
+    return this.activeCell$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((cell: number) => {
+        if (this.cellId !== cell) {
+          return;
+        }
+        this.startTimer()
+        this.renderColor('#d5ba00');
+      })
+  }
+
+  private initValues(): void {
+    this.playerCells = this.cellService.playerCells;
+    this.computerCells = this.cellService.computerCells;
+  }
+}
