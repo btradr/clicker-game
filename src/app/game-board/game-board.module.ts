668a3fef174e9c6a755cd48adced60b6f1e5ffda
Andrew Baturin <baturin.andrew1501@gmail.com>
2022-12-30 00:35:36 +0200

    feat(INITIALIZER): added INITIALIZER for building cells

--- src/app/game-board/game-board.module.ts
+++ src/app/game-board/game-board.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Components
 import { GameBoardComponent } from './components/game-board/game-board.component';
+
+// Services
+import { CellService } from '../shared/services/cell.service';
 
 @NgModule({
   declarations: [
     GameBoardComponent
   ],
+  imports: [
+    CommonModule
+  ],
   exports: [
     GameBoardComponent
   ],
-  imports: [
-    CommonModule
+  providers: [
+    CellService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (service: CellService) => async () => {
+        return service.createCells();
+      },
+      deps: [CellService],
+      multi: true
+    }
   ]
 })
 export class GameBoardModule { }
