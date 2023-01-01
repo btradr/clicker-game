3bc06462c019bb724bf7a539e6cbd5cebf86cd1e
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-01-01 16:48:48 +0200

    feat(constants.js): create constants-file

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,8 @@
+export const AMOUNT_TO_WIN: number = 10;
+
+export const GREEN_BACKGROUND: string = '#359b14';
+export const RED_BACKGROUND: string = '#ab0404';
+export const YELLOW_BACKGROUND: string = '#d5ba00';
+
+export const STORAGE_PLAYER_KEY: string = 'playerScore';
+export const STORAGE_COMPUTER_KEY: string = 'computerScore';
